469a71e1a9604672ece92a8028ea89a396d99dbb
Victor Marks <vjmarks@gmail.com>
2019-08-13 00:22:06 -0700

    changed background color of user answer if guessed correctly

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,42 +1,50 @@
 <template>
   <div class="question-box-container">
     <b-jumbotron>
-      <template slot="lead">Some question here?</template>
-      {{ currentQuestion.question }}
+      <template slot="lead">{{ currentQuestion.question }}</template>
+
       <hr class="my-4" />
 
       <b-list-group>
         <b-list-group-item
-          v-for="(answer, index) in answers"
+          v-for="(answer, index) in shuffledAnswers"
           :key="index"
           @click.prevent="selectAnswer(index)"
-          :class="[selectedIndex === index ? 'selected' : '']"
-        >{{answer}}</b-list-group-item>
+          :class="answerClass(index)"
+        >{{ answer }}</b-list-group-item>
       </b-list-group>
 
-      <b-button variant="primary" @click="submitAnswer">Submit</b-button>
-      <b-button @click="next" variant="success" href="#">Next</b-button>
+      <b-button
+        variant="primary"
+        @click="submitAnswer"
+        :disabled="selectedIndex === null || answered"
+      >Submit</b-button>
+      <b-button @click="next" variant="success">Next</b-button>
     </b-jumbotron>
   </div>
 </template>
 
 <script>
 import _ from "lodash";
-
 export default {
   props: {
     currentQuestion: Object,
     next: Function,
     increment: Function
   },
-  data() {
+  data: function() {
     return {
       selectedIndex: null,
-      shuffledAnswers: []
+      correctIndex: null,
+      shuffledAnswers: [],
+      answered: false
     };
   },
   computed: {
     answers() {
+      // this function is no longer used in finished code
+      // it is replaced by the watch function below and the
+      // shuffleAnswers method
       let answers = [...this.currentQuestion.incorrect_answers];
       answers.push(this.currentQuestion.correct_answer);
       return answers;
@@ -47,6 +55,7 @@ export default {
       immediate: true,
       handler() {
         this.selectedIndex = null;
+        this.answered = false;
         this.shuffleAnswers();
       }
     }
@@ -57,11 +66,10 @@ export default {
     },
     submitAnswer() {
       let isCorrect = false;
-
       if (this.selectedIndex === this.correctIndex) {
         isCorrect = true;
       }
-
+      this.answered = true;
       this.increment(isCorrect);
     },
     shuffleAnswers() {
@@ -70,6 +78,24 @@ export default {
         this.currentQuestion.correct_answer
       ];
       this.shuffledAnswers = _.shuffle(answers);
+      this.correctIndex = this.shuffledAnswers.indexOf(
+        this.currentQuestion.correct_answer
+      );
+    },
+    answerClass(index) {
+      let answerClass = "";
+      if (!this.answered && this.selectedIndex === index) {
+        answerClass = "selected";
+      } else if (this.answered && this.correctIndex === index) {
+        answerClass = "correct";
+      } else if (
+        this.answered &&
+        this.selectedIndex === index &&
+        this.correctIndex !== index
+      ) {
+        answerClass = "incorrect";
+      }
+      return answerClass;
     }
   }
 };
@@ -79,24 +105,19 @@ export default {
 .list-group {
   margin-bottom: 15px;
 }
-
 .list-group-item:hover {
   background: #eee;
   cursor: pointer;
 }
-
 .btn {
   margin: 0 5px;
 }
-
 .selected {
   background-color: lightblue;
 }
-
 .correct {
   background-color: lightgreen;
 }
-
 .incorrect {
   background-color: red;
 }
